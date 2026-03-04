d67031e4af6e8308b80e8dbc1153c5a9da161b03
mczhu <mczhu@mczhudeMac-mini.local>
2026-03-04 10:49:55 +0800

    完善逻辑

--- weichat-api/src/main/java/com/weichat/api/vo/response/cdn/CdnUploadResponse.java
+++ weichat-api/src/main/java/com/weichat/api/vo/response/cdn/CdnUploadResponse.java
@@ -13,27 +13,42 @@ import lombok.Data;
 @ApiModel(description = "CDN上传响应数据")
 public class CdnUploadResponse {
 
-    @ApiModelProperty(value = "CDN Key", example = "cdn_key_xxx")
-    private String cdnKey;
+    @ApiModelProperty(value = "CDN Key", example = "306b020102046430620201000204cca06f0602030f424102040bc16eb4020469a7998a042439656466633563652d343335612d343030322d396331642d3830306164666465336237650203102800020301ced0041000dd41b689a291dbe885035a5fbdcb390201010201000400")
+    private String cdn_key;
 
-    @ApiModelProperty(value = "AES Key", example = "aes_key_xxx")
-    private String aesKey;
+    @ApiModelProperty(value = "缩略图高度", example = "387")
+    private Integer thumb_image_height;
 
-    @ApiModelProperty(value = "文件MD5", example = "d41d8cd98f00b204e9800998ecf8427e")
-    private String md5;
+    @ApiModelProperty(value = "中图大小", example = "95287")
+    private Integer mid_image_size;
 
-    @ApiModelProperty(value = "文件大小(字节)", example = "102400")
-    private Integer fileSize;
+    @ApiModelProperty(value = "文件大小", example = "118466")
+    private Integer size;
 
-    @ApiModelProperty(value = "文件URL(部分接口返回)", example = "https://cdn.example.com/file.jpg")
-    private String fileUrl;
+    @ApiModelProperty(value = "缩略图宽度", example = "659")
+    private Integer thumb_image_width;
 
-    @ApiModelProperty(value = "宽度(图片/视频)", example = "800")
+    @ApiModelProperty(value = "AES Key", example = "1691E8F3D57F652AF9A26ABD26C2DB8E")
+    private String aes_key;
+
+    @ApiModelProperty(value = "宽度", example = "2196")
     private Integer width;
 
-    @ApiModelProperty(value = "高度(图片/视频)", example = "600")
+    @ApiModelProperty(value = "缩略图文件大小", example = "16599")
+    private Integer thumb_file_size;
+
+    @ApiModelProperty(value = "缩略图MD5", example = "7b96dd982d40484a7fb79489caca4444")
+    private String thumb_file_md5;
+
+    @ApiModelProperty(value = "文件ID", example = "306b020102046430620201000204cca06f0602030f424102040bc16eb4020469a7998a042439656466633563652d343335612d343030322d396331642d3830306164666465336237650203102800020301ced0041000dd41b689a291dbe885035a5fbdcb390201010201000400")
+    private String fileid;
+
+    @ApiModelProperty(value = "文件MD5", example = "00dd41b689a291dbe885035a5fbdcb39")
+    private String md5;
+
+    @ApiModelProperty(value = "高度", example = "1289")
     private Integer height;
 
-    @ApiModelProperty(value = "时长(音频/视频,秒)", example = "60")
-    private Integer duration;
+    @ApiModelProperty(value = "返回码", example = "0")
+    private String retcode;
 }
