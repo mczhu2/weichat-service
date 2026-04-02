fb965556dc07d9f17cbb999ffcc86aaebdeecdc3
MengChun.Zhu <MengChun.Zhu>
2026-04-02 15:26:42 +0800

    完善逻辑

--- weichat-api/src/main/java/com/weichat/api/vo/response/init/QrCodeResponse.java
+++ weichat-api/src/main/java/com/weichat/api/vo/response/init/QrCodeResponse.java
@@ -16,6 +16,9 @@ public class QrCodeResponse {
     @ApiModelProperty(value = "二维码Base64编码", example = "data:image/png;base64,iVBORw0KGgo...")
     private String qrCode;
 
+    @ApiModelProperty(value = "二维码Key-后续验证码需要附带填写", example = "78D0489F92F7DDFE2FD73CD266473248")
+    private String Key;
+
     @ApiModelProperty(value = "二维码状态(0:等待扫码, 1:已扫码待确认, 2:已确认)", example = "0")
     private Integer status;
 
