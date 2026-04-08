de54b8a1a0317565dbea72e9ae779154fea0c414
MengChun.Zhu <MengChun.Zhu>
2026-04-08 19:45:50 +0800

    chore: increase rest template timeouts

--- weichat-api/src/main/java/com/weichat/api/config/RestTemplateConfig.java
+++ weichat-api/src/main/java/com/weichat/api/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.weichat.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
@@ -7,12 +8,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-    
+
+    @Value("${rest.template.connect-timeout-ms:30000}")
+    private int connectTimeoutMs;
+
+    @Value("${rest.template.read-timeout-ms:120000}")
+    private int readTimeoutMs;
+
     @Bean
     public RestTemplate restTemplate() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setConnectTimeout(10000);
-        factory.setReadTimeout(30000);
+        factory.setConnectTimeout(connectTimeoutMs);
+        factory.setReadTimeout(readTimeoutMs);
         return new RestTemplate(factory);
     }
 }
